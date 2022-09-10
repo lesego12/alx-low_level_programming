bf82139bfcf4f033a6f98d1fc681e6181aadd54d
lesego12 <nkosilesego93@gmail.com>
2022-09-09 18:05:54 -0700

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single-digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n< 10; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',')
+			putchar(' ')
+		}
+	}
+	putchar('\n');
+	return (0);
+}
