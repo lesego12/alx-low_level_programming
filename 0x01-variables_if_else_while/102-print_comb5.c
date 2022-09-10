5294dbd7417024b3648d0806720a14b07e03eeb4
lesego12 <nkosilesego93@gmail.com>
2022-09-09 19:23:54 -0700

    Print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 		{
 			if (a < b)
 			{
-				putchar((a / 10) + 58);
-				putchar((a % 10) + 58);
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
 				putchar(' ');
-				putchar((b / 10) + 58);
-				putchar((b % 10) + 58);
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
 				if (a != 98 || b != 99)
 				{
 					putchar(',');
