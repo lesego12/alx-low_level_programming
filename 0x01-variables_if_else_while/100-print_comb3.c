4e7c67a84beac06d05e2dad4990da682fb7ba487
lesego12 <nkosilesego93@gmail.com>
2022-09-09 18:37:08 -0700

    Print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int a, b;
-	
+
 	for (a = 48; a <= 56; a++)
 	{
 		for (b = 49; b <= 57; b++)
@@ -16,7 +16,7 @@ int main(void)
 			{
 				putchar(a);
 				putchar(b);
-				if (a !=56 || b !=57)
+				if (a != 56 || b != 57)
 				{
 					putchar(',');
 					putchar(' ');
