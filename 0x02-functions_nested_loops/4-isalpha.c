add11dba1904c32ec9cd13374d21380129c2a44b
lesego12 <nkosilesego93@gmail.com>
2022-09-13 15:17:33 -0700

    Is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * Return: 1 if c is a letter, lowercase or uppercase,
  * 0 otherwise
  */
-int isalpha(int c)
+int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
