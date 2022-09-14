39e46c7feb459fdb544c20872b7d473536cf0998
lesego12 <nkosilesego93@gmail.com>
2022-09-13 17:26:35 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@
 void jack_bauer(void)
 {
 	int i;
- 	int j;
+	int j;
 
 	i = 0;
 
@@ -19,7 +19,7 @@ void jack_bauer(void)
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((j /10) + '0');
+			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
