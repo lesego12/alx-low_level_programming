94946d06803947898cea7ced49f9994faf84501d
lesego12 <nkosilesego93@gmail.com>
2022-09-13 17:07:58 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,27 +3,26 @@
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59
- * @b: seconds
  */
 void jack_bauer(void)
 {
-	int a; b;
+	int i; j;
 
-	a = 0;
+	i = 0;
 
-	while (a < 24)
+	while (i < 24)
 	{
-		b = 0;
-		while (b < 60)
+		j = 0;
+		while (j < 60)
 		{
-			_putchar((a / 10) + '0');
-			_putchar((a % 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((b /10) + '0');
-			_putchar((b % 10) + '0');
+			_putchar((j /10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
-			b++;
+			j++;
 		}
-		a++;
+		i++;
 	}
 }
