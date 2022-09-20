c5e5bd1707b373bd19e02428dad6fdcf96f4036e
lesego12 <nkosilesego93@gmail.com>
2022-09-19 18:06:41 -0700

    Puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,18 +14,19 @@ void puts_half(char *str)
 	{
 		a++;
 	}
-	 if (a % 2 == 0)
-	 {
-		 for (c = a / 2; str[c] != '\0'; c++);
-		 {
-			 _putchar(str[c]);
-		 }
-	 } else if (c % 2)
-	 {
-		 for (b = (a - 1) / 2; b < a - 1; b++)
-		 {
-			 _putchar(str[b + 1]);
-		 }
-	 }
-	 _putchar('\n');
+	
+	if (a % 2 == 0)
+	{
+		for (c = a / 2; str[c] != '\0'; c++)
+		{
+			_putchar(str[c]);
+		}
+	} else if (a % 2)
+	{
+		for (b = (a - 1) / 2; b < a - 1; b++)
+		{
+			_putchar(st[b + 1]);
+		}
+	}
+	_putchar('\n');
 }
