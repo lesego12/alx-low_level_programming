7e414e0d5d75958a8e1e4744191eb4bdada26315
lesego12 <nkosilesego93@gmail.com>
2022-09-21 16:41:13 -0700

    String concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,17 +15,12 @@ char *_strcat(char *dest, char *src)
 	a = 0;
 	b = 0;
 
-	while (dest[a] != '\0')
+	while (dest[a])
 		a++;
 
 	for (b = 0; src[b]; b++)
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
 
-	dest[a] = '\0';
+		dest[a++] = src[b];
 
 	return (dest);
 }
