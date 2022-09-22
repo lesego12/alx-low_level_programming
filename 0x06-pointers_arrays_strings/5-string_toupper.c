b97f1a81e6e5dd84716d5fe2f2cc118b939ef4ac
lesego12 <nkosilesego93@gmail.com>
2022-09-22 16:03:22 -0700

    String to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,11 +4,12 @@
  * string_toupper - changes all lowercase letters of a string
  * to uppercase
  * @s: string to be modified
+ * Return: the resulting string
  */
 char *string_toupper(char *s)
 {
 	int i;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
