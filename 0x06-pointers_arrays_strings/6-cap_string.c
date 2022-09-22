e0ae27046a21f74c2e1bb103bd0945376f743ecc
lesego12 <nkosilesego93@gmail.com>
2022-09-22 16:39:20 -0700

    Capitalizes string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string to be modified
+ * Return: the resulting string
+ */
+char *cap_string(char *s)
+{
+	int a;
+	int b;
+
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'}
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (a == 0 && s[a] >= 'a' && s[a] <= 'z')
+			s[i] -= 32;
+
+		for (b = 0; b < 13; b++)
+		{
+			if (s[a] == spe[b])
+			{
+				if (s[a + 1] >= 'a' && s[a + 1] <= 'z')
+				{
+					s[a + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -9,3 +9,5 @@ File 3-strcmp.c is a function that compares two strings.
 File 4-rev_array.c a function taht reverses the content of an array of integers.
 
 File 5-string_toupper.c is a function that changes all lowercase letters of a string to uppercase.
+
+File 6-cap_string.c is a function that capitalizes all words of a string.
