180af6099d8db4d739656f0b4593f68848c20094
lesego12 <nkosilesego93@gmail.com>
2022-09-22 12:52:14 -0700

    String and copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,12 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (src[i++])
 		dl++;
-	
+
 	for (i = 0; src[i] && i < n; i++)
 		dest[i] = src[i];
 
 	for (i = dl; i < n; i++)
 		dest[i] = '\0';
-	
+
 	return (dest);
 }
