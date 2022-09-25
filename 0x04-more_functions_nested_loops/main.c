5be426c1885a3064a03e674fb0e38ce89b4955ea
lesego12 <nkosilesego93@gmail.com>
2022-09-25 15:58:36 -0700

    Print square

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 
 
@@ -16,14 +16,12 @@ int main(void)
 
 {
 
-	    print_diagonal(0);
+	    print_square(2);
 
-	        print_diagonal(2);
+	        print_square(10);
 
-		    print_diagonal(10);
+		    print_square(0);
 
-		        print_diagonal(-4);
-
-			    return (0);
+		        return (0);
 
 }
