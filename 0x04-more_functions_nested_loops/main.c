89f0c03f5aa6e626ecaefa95c15b84846e99fe86
lesego12 <nkosilesego93@gmail.com>
2022-09-25 16:02:48 -0700

    Print triangle

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 
 
@@ -16,12 +16,14 @@ int main(void)
 
 {
 
-	    print_square(2);
+	    print_triangle(2);
 
-	        print_square(10);
+	        print_triangle(10);
 
-		    print_square(0);
+		    print_triangle(1);
 
-		        return (0);
+		        print_triangle(0);
+
+			    return (0);
 
 }
