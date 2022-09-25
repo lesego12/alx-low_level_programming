e489d33b441b0cb5f0fa191c9e3a9347bba80c1b
lesego12 <nkosilesego93@gmail.com>
2022-09-25 15:49:46 -0700

    Print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
+ * @n: number of times the character should be printed
  */
 void print_line(int n)
 {
