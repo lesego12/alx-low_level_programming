aada1ad817e3df0c0347a660a0787e4381ff3987
lesego12 <nkosilesego93@gmail.com>
2022-09-25 15:53:18 -0700

    Print diagonal

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -16,13 +16,13 @@ int main(void)
 
 {
 
-	    print_line(0);
+	    print_diagonal(0);
 
-	        print_line(2);
+	        print_diagonal(2);
 
-		    print_line(10);
+		    print_diagonal(10);
 
-		        print_line(-4);
+		        print_diagonal(-4);
 
 			    return (0);
 
