53a62ffda2a6d92548a6941dce0296a02110900c
lesego12 <nkosilesego93@gmail.com>
2022-09-26 16:58:27 -0700

    String lenth

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,10 +9,9 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	int j;
 
-	i = 0;
 
 	while (*s)
 	{
@@ -21,7 +20,9 @@ unsigned int _strspn(char *s, char *accept)
 			if (*s == accept[j])
 			{
 				i++;
+				break;
 			}
+
 			else if (accept[j + 1] == '\0')
 				return (i);
 		}
