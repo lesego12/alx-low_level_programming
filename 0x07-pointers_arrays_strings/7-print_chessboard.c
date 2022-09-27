e8b4a9a8c8c25b0763bdd0a06affc1d24a7ec91c
lesego12 <nkosilesego93@gmail.com>
2022-09-26 18:42:56 -0700

    Print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,16 +9,12 @@ void print_chessboard(char (*a)[8])
 	int i;
 	int j;
 
-	i = j = 0;
-	while (i < 8)
+	for (i = 0; i < 8; i++)
 	{
-		j = 0;
-		while (j < 8)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			j++;
 		}
 		_putchar('\n');
-		i++;
 	}
 }
