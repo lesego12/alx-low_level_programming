a586f8af6bf2895c8e3e9760c01c12356a62bd13
lesego12 <nkosilesego93@gmail.com>
2022-09-28 11:01:30 -0700

    Power recursion

--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -5,3 +5,5 @@ FILE 1-print_rev_recursion.c is a function that prints a string in reverse.
 FILE 2-strlen_recursion.c is a function that returns the length of a string.
 
 FILE 3-factorial.c is a function that returns the factorial of a given number.
+
+FILE 4-pow_recursion.c function that returns the value of x raised to the power of y.
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -22,22 +22,30 @@ int main(void)
 
 
 
-	        r = factorial(1);
+	        r = _pow_recursion(1, 10);
 
 		    printf("%d\n", r);
 
-		        r = factorial(5);
+		        r = _pow_recursion(1024, 0);
 
 			    printf("%d\n", r);
 
-			        r = factorial(10);
+			        r = _pow_recursion(2, 16);
 
 				    printf("%d\n", r);
 
-				        r = factorial(-1024);
+				        r = _pow_recursion(5, 2);
 
 					    printf("%d\n", r);
 
-					        return (0);
+					        r = _pow_recursion(5, -2);
+
+						    printf("%d\n", r);
+
+						        r = _pow_recursion(-5, 3);
+
+							    printf("%d\n", r);
+
+							        return (0);
 
 }
