1273ca2090bb420bb73d4d5d29a24a956cee0ad2
lesego12 <nkosilesego93@gmail.com>
2022-10-06 16:09:39 -0700

    Calloc

--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,5 @@
 FILE 0-malloc_checked.c is a function that allocates memory using malloc.
 
 FILE 1-string_nconcat.c is a function that concatenates two strings.
+
+FILE 2-calloc.c is a function that allovates memory for an array, using malloc.
--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -4,6 +4,64 @@
 
 #include <stdlib.h>
 
+#include <string.h>
+
+
+
+/**
+ *
+ *  * simple_print_buffer - prints buffer in hexa
+ *
+ *   * @buffer: the address of memory to print
+ *
+ *    * @size: the size of the memory to print
+ *
+ *     *
+ *
+ *      * Return: Nothing.
+ *
+ *       */
+
+void simple_print_buffer(char *buffer, unsigned int size)
+
+{
+
+	    unsigned int i;
+
+
+
+	        i = 0;
+
+		    while (i < size)
+
+			        {
+
+					        if (i % 10)
+
+							        {
+
+									            printf(" ");
+
+										            }
+
+						        if (!(i % 10) && i)
+
+								        {
+
+										            printf("\n");
+
+											            }
+
+							        printf("0x%02x", buffer[i]);
+
+								        i++;
+
+									    }
+
+		        printf("\n");
+
+}
+
 
 
 /**
@@ -20,16 +78,22 @@ int main(void)
 
 {
 
-	    char *concat;
+	    char *a;
 
 
 
-	        concat = string_nconcat("Best ", "School !!!", 6);
+	        a = _calloc(98, sizeof(char));
 
-		    printf("%s\n", concat);
+		    strcpy(a, "Best");
 
-		        free(concat);
+		        strcpy(a + 4, " School! :)\n");
 
-			    return (0);
+			    a[97] = '!';
+
+			        simple_print_buffer(a, 98);
+
+				    free(a);
+
+				        return (0);
 
 }
