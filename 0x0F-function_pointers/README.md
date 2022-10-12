dd1b8b42102f8e9bac24e02215bc153507e167c0
lesego12 <nkosilesego93@gmail.com>
2022-10-12 11:20:57 -0700

    Int index

--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -2,4 +2,4 @@ FILE 0-print_name.c is a function that prints a name.
 
 FILE 1-array_iterator.c is  a function that executes a function given as a parameter on each element of an array.
 
-FILE 
+FILE 2-int_index.c is  a function that searches for an integer. 
--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -6,21 +6,21 @@
 
 /**
  *
- *  * print_elem - prints an integer
+ *  * is_98 - check if a number is equal to 98
  *
- *   * @elem: the integer to print
+ *   * @elem: the integer to check
  *
  *    *
  *
- *     * Return: Nothing.
+ *     * Return: 0 if false, something else otherwise.
  *
  *      */
 
-void print_elem(int elem)
+int is_98(int elem)
 
 {
 
-	    printf("%d\n", elem);
+	    return (98 == elem);
 
 }
 
@@ -28,21 +28,45 @@ void print_elem(int elem)
 
 /**
  *
- *  * print_elem_hex - prints an integer, in hexadecimal
+ *  * is_strictly_positive - check if a number is greater than 0
  *
- *   * @elem: the integer to print
+ *   * @elem: the integer to check
  *
  *    *
  *
- *     * Return: Nothing.
+ *     * Return: 0 if false, something else otherwise.
  *
  *      */
 
-void print_elem_hex(int elem)
+int is_strictly_positive(int elem)
 
 {
 
-	    printf("0x%x\n", elem);
+	    return (elem > 0);
+
+}
+
+
+
+
+
+/**
+ *
+ *  * abs_is_98 - check if the absolute value of a number is 98
+ *
+ *   * @elem: the integer to check
+ *
+ *    *
+ *
+ *     * Return: 0 if false, something else otherwise.
+ *
+ *      */
+
+int abs_is_98(int elem)
+
+{
+
+	    return (elem == 98 || -elem == 98);
 
 }
 
@@ -62,14 +86,24 @@ int main(void)
 
 {
 
-	    int array[5] = {0, 98, 402, 1024, 4096};
+	    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+
+	        int index;
 
 
 
-	        array_iterator(array, 5, &print_elem);
+		    index = int_index(array, 20, is_98);
 
-		    array_iterator(array, 5, &print_elem_hex);
+		        printf("%d\n", index);
 
-		        return (0);
+			    index = int_index(array, 20, abs_is_98);
+
+			        printf("%d\n", index);
+
+				    index = int_index(array, 20, is_strictly_positive);
+
+				        printf("%d\n", index);
+
+					    return (0);
 
 }
