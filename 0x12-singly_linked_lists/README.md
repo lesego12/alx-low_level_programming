d9dad7ff6185110710e58005bf148cf11a7872fc
lesego12 <nkosilesego93@gmail.com>
2022-10-22 03:21:28 -0700

    First

--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -7,3 +7,5 @@ FILE 2-add_node.c is a function that adds a new node at the beginning of a list_
 FILE 3-add_node_end.c is a function that adds a new node at the end of a list_t list.
 
 FILE 4-free_list.c is  a function that frees a list_t list.
+
+FILE 100-first.c is  a function that prints You're beat! and yet, you must allow,\nI bore my house upon my back!\n before the main function is executed.
--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -1,10 +1,4 @@
-#include <stdlib.h>
-
-#include <string.h>
-
 #include <stdio.h>
-
-#include "lists.h"
 
 
 
@@ -22,28 +16,8 @@ int main(void)
 
 {
 
-	    list_t *head;
+	    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 
-
-
-	        head = NULL;
-
-		    add_node_end(&head, "Bob");
-
-		        add_node_end(&head, "&");
-
-			    add_node_end(&head, "Kris");
-
-			        add_node_end(&head, "love");
-
-				    add_node_end(&head, "asm");
-
-				        print_list(head);
-
-					    free_list(head);
-
-					        head = NULL;
-
-						    return (0);
+	        return (0);
 
 }
