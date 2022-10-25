13411aab58d6f65bd9b360f3afd630430c2b1a1d
lesego12 <nkosilesego93@gmail.com>
2022-10-25 12:50:15 -0700

    Add node

--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -24,40 +24,28 @@ int main(void)
 
 	    listint_t *head;
 
-	        listint_t *new;
-
-		    listint_t hello = {8, NULL};
-
-		        size_t n;
 
 
+	        head = NULL;
 
-			    head = &hello;
+		    add_nodeint(&head, 0);
 
-			        new = malloc(sizeof(listint_t));
+		        add_nodeint(&head, 1);
 
-				    if (new == NULL)
+			    add_nodeint(&head, 2);
 
-					        {
+			        add_nodeint(&head, 3);
 
-							        printf("Error\n");
+				    add_nodeint(&head, 4);
 
-								        return (1);
+				        add_nodeint(&head, 98);
 
-									    }
+					    add_nodeint(&head, 402);
 
-				        new->n = 9;
+					        add_nodeint(&head, 1024);
 
-					    new->next = head;
+						    print_listint(head);
 
-					        head = new;
-
-						    n = listint_len(head);
-
-						        printf("-> %lu elements\n", n);
-
-							    free(new);
-
-							        return (0);
+						        return (0);
 
 }
