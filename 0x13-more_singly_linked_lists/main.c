f4bfa3ab4273d02f5112e417b6844ffa51843653
lesego12 <nkosilesego93@gmail.com>
2022-10-25 17:43:06 -0700

    Insert node

--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -24,34 +24,36 @@ int main(void)
 
 	    listint_t *head;
 
-	        int sum;
 
 
+	        head = NULL;
 
-		    head = NULL;
+		    add_nodeint_end(&head, 0);
 
-		        add_nodeint_end(&head, 0);
+		        add_nodeint_end(&head, 1);
 
-			    add_nodeint_end(&head, 1);
+			    add_nodeint_end(&head, 2);
 
-			        add_nodeint_end(&head, 2);
+			        add_nodeint_end(&head, 3);
 
-				    add_nodeint_end(&head, 3);
+				    add_nodeint_end(&head, 4);
 
-				        add_nodeint_end(&head, 4);
+				        add_nodeint_end(&head, 98);
 
-					    add_nodeint_end(&head, 98);
+					    add_nodeint_end(&head, 402);
 
-					        add_nodeint_end(&head, 402);
+					        add_nodeint_end(&head, 1024);
 
-						    add_nodeint_end(&head, 1024);
+						    print_listint(head);
 
-						        sum = sum_listint(head);
+						        printf("-----------------\n");
 
-							    printf("sum = %d\n", sum);
+							    insert_nodeint_at_index(&head, 5, 4096);
 
-							        free_listint2(&head);
+							        print_listint(head);
 
-								    return (0);
+								    free_listint2(&head);
+
+								        return (0);
 
 }
