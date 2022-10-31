21ee37a644e4ab34f7b2c164f8f93989c9903dc7
lesego12 <nkosilesego93@gmail.com>
2022-10-31 13:11:39 -0700

    Create file

--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,1 +1,3 @@
 FILE 0-read_textfile.c is a function that reads a text file and prints it to the POSIX standard output.
+
+FILE 1-create_file.c is a function that creates a file.
--- 0x15-file_io/main.c
+++ 0x15-file_io/main.c
@@ -20,28 +20,24 @@ int main(int ac, char **av)
 
 {
 
-	    ssize_t n;
+	    int res;
 
 
 
-	        if (ac != 2)
+	        if (ac != 3)
 
 			    {
 
-				            dprintf(2, "Usage: %s filename\n", av[0]);
+				            dprintf(2, "Usage: %s filename text\n", av[0]);
 
 					            exit(1);
 
 						        }
 
-		    n = read_textfile(av[1], 114);
+		    res = create_file(av[1], av[2]);
 
-		        printf("\n(printed chars: %li)\n", n);
+		        printf("-> %i)\n", res);
 
-			    n = read_textfile(av[1], 1024);
-
-			        printf("\n(printed chars: %li)\n", n);
-
-				    return (0);
+			    return (0);
 
 }
