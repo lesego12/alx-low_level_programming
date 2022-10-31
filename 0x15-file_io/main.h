a24d001613fdf193cd2f68f165ea906b34e8cae4
lesego12 <nkosilesego93@gmail.com>
2022-10-31 13:03:05 -0700

    Read text file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stddef.h> 
+#include <stddef.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
