0627356ecae869de3c48b2d375345cfae1299f2e
lesego12 <nkosilesego93@gmail.com>
2022-12-12 11:58:24 -0800

    Main

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -11,7 +11,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *s)
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -24,5 +24,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+extern div_t div(int __numer, int __denom);
 
 #endif
